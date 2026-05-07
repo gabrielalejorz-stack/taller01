3149263ba63d8c7195745534b212e6724eab3f70
gabriel <monteroluz979@gmail.com>
2026-05-07 17:04:01 -0500

    trabajo 1

---
+++ java script/ejer.js
@@ -0,0 +1,7 @@
+n = 11
+if (n<0)
+    console.log("el nuemro ed negativo")
+else if ( n <=10)
+    console.log("el numero esta entre 0 y 10")
+else
+    console.log("el numero es mayor a 10")
--- python/ejer.py
+++ python/ejer.py
@@ -1,1 +1,2 @@
-print("hola mundo)")
+numero= 10
+print(numero)
