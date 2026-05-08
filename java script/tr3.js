e06088b026fb06c80869db4d5e769dd9f8a5f13c
gabriel <monteroluz979@gmail.com>
2026-05-08 17:31:33 -0500

    c

---
+++ java script/tr3.js
@@ -0,0 +1,11 @@
+let num1=parseFloat(prompt("ingrese primer numero"));
+let num2=ParseFloat(prompt("ingrese segundo numero");
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+
+alert(
+    "suma: "+ suma +
+    "/resta": + resta +
+)
---
+++ python/tr3.py
@@ -0,0 +1,17 @@
+num1=float(input("coloque el primer numero"))
+num2=float(input("coloque el segundo numero"))
+
+suma= num1 + num2
+resta= num1 - num2
+multiplicacion= num1 * num2
+
+print("suma:", suma)
+print("resta:", resta)
+print("multiplacion:", multiplicacion)
+
+if num2== 0:
+    print("error no se puede divir entre 0")
+else:
+    division=num1 / num2
+    print("division:", division)
+
