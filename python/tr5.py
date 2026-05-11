fe57797f5d94e22827c98a8001e481632ef0fb27
gabriel <monteroluz979@gmail.com>
2026-05-11 17:43:13 -0500

    g

---
+++ python/tr5.py
@@ -0,0 +1,4 @@
+radio = float(input("Ingrese el radio del círculo: "))
+pi = 3.1416
+area = pi * (radio ** 2)
+print("El área del círculo es:", area)
---
+++ python/tr7.py
@@ -0,0 +1,4 @@
+a = float(input("Número 1: "))
+b = float(input("Número 2: "))
+c = float(input("Número 3: "))
+print("Promedio:", (a + b + c) / 3)
