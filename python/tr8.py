a8928b105444bf02d3bde5e4acdd85a83ce8d8aa
gabriel <monteroluz979@gmail.com>
2026-05-12 15:20:52 -0500

    f

---
+++ python/tr8.py
@@ -0,0 +1,6 @@
+horas=float(input("ingrese el valor de la horas trabajadas"))
+valor_hora=float(input("ingrese el valor por hora"))
+
+salario_total= horas*valor_hora
+
+print("el salario total es :",salario_total,)
---
+++ python/tr9.py
@@ -0,0 +1,15 @@
+a = input("digite el primer valor")
+b= input("digite el segundo valor")
+
+print("despues del cambio")
+print("a=",a)
+print("b=",b)
+
+a,b=b,a
+
+print("despues del intercambio:")
+print("a =",a)
+print("b =",b)
+
+
+
