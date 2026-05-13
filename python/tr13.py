bad2eedac33a2d69909b0454c8cb7d63ddc7f5c9
gabrielalejorz-stack <gabrielalejorz@gmail.com>
2026-05-13 13:19:22 -0500

    m

---
+++ python/tr13.py
@@ -0,0 +1,9 @@
+num1=int(input("digite el pirmer numero"))
+num2=int(input("digite el segundo numero"))
+
+if num1 > num2:
+ print("el numero es mayor")
+elif num2> num1:
+ print("el numero es menor")
+else:
+ print("los numeros son iguales")
