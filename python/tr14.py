111631a2d2944d469771f88b576d2734786cdd49
gabriel <monteroluz979@gmail.com>
2026-05-13 16:45:41 -0500

    n

---
+++ python/tr14.py
@@ -0,0 +1,11 @@
+num1=int(input("ingre el primer numero"))
+num2=int(input("ingrese el seguno numero"))
+num3=int(input("ingrese el tercer numero"))
+
+num=num1
+
+if num2>num:
+    num2=num
+if num3>num:
+    num3>num
+print("el numero mayor es:",num)
