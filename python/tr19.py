85c470657cbc539b6f91579ad8eef5c49e3ee80e
karen <monteroluz979@gmail.com>
2026-05-20 17:35:06 -0500

    tr 21

--- python/tr19.py
+++ python/tr19.py
@@ -1,1 +1,14 @@
 ingresos=float(input("ingrese sus ingresos"))
+if ingresos<=1500000:
+    impuesto=0
+elif ingresos<=3000000:
+    impuesto=ingresos *0.10
+else:
+    ingresos>=3000000
+impuesto=ingresos *0.20
+
+neto=ingresos-impuesto
+print("\n--- Resultado ---")
+print(f"Salario: ${salario:,.0f}")
+print(f"Impuesto: ${impuesto:,.0f}")
+print(f"Salario neto: ${neto:,.0f}")
