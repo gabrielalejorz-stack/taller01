d439a16c8f383a5c1659e0cfc5273c3a0488b268
gabrielalejorz-stack <gabrielalejorz@gmail.com>
2026-05-24 18:41:50 -0500

    35

--- python/tr10.py
+++ python/tr10.py
@@ -3,4 +3,4 @@ num2=float(input("digite el segundo numero"))
 
 division = num1 / num2
 
-print("la division es:",division)
+print(division)
---
+++ python/tr34.py
@@ -0,0 +1,5 @@
+N = int(input("Ingrese un número: "))
+suma = 0
+for i in range(1, N + 1):
+    suma += i
+print(suma)
